e3bc02e931a0b14d4b424252c73b84b973f2fcf8
Brent Kolasinski <brent@0x626b.com.com>
2020-05-16 19:50:31 -0500

    added forget and pruning

--- restic_backup.sh
+++ restic_backup.sh
@@ -6,8 +6,16 @@ B2_ACCOUNT_KEY=<backblaze appkey key>
 RESTIC_PASSWORD_FILE="/path/to/restic/repo/pass"
 RESTIC_REPO="<backblaze b2 repo/repo"
 RESTIC_UPLOAD_LIMIT="500000"
+RESTIC_KEEP_LAST_SNAPSHOTS="4"
 
 EXCLUDE_LIST="/path/to/excludes/file"
 LOCAL_DIR="/path/to/local/dir/to/backup"
 
+
+# Delete any unecessary snapshots and prune
+restic -r $RESTIC_REPO -p $RESTIC_PASSWORD_FILE forget --keep-last $RESTIC_KEEP_LAST_SNAPSHOTS
+restic -r $RESTIC_REPO -p $RESTIC_PASSWORD_FILE prune
+
+# Run the backup
 restic -r $RESTIC_REPO -p $RESTIC_PASSWORD_FILE --exclude-file $EXCLUDE_LIST backup $LOCAL_DIR
+
